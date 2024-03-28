1616371d27a6d82ee133307b10ab58e0a01685ef
Tanvir Ahmed Fahim <tfahim00@gmail.com>
2024-03-29 02:58:59 +0600

    Expense_manager

---
+++ NSTU_Ind_Attach_Fahim-9fe34e616d1cd578bb2a5517cf5f60d73eef62f8/Frontend/Expense_Manager/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import ExpensePage from './component/Expense_Page'
+
+function App() {
+  
+
+  return (
+   <div>
+      <ExpensePage>
+
+      </ExpensePage>
+   </div>
+  )
+}
+
+export default App
